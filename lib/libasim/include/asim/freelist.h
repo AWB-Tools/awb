0af3b53b2346d8dd00b72441d74d611b60098ed1
Sailashri Parthasarathy <sailashri.parthasarathy@intel.com>
2007-03-30 22:26:02 +0000

    CSN: CSN-asimcore-70
    Missed a file in rev. 68.
    
    Updating with simcore changes (rev. 2286)
    
    - basic rsync script; for private use currently.

--- lib/libasim/include/asim/freelist.h
+++ lib/libasim/include/asim/freelist.h
@@ -1,17 +1,20 @@
-/**************************************************************************
- * INTEL CONFIDENTIAL Copyright (c) 2007 Intel Corp.
+/*
+ * Copyright (C) 2003-2006 Intel Corporation
  *
- * Recipient is granted a non-sublicensable copyright license under
- * Intel copyrights to copy and distribute this code internally only.
- * This code is provided "AS IS" with no support and with no
- * warranties of any kind, including warranties of MERCHANTABILITY,
- * FITNESS FOR ANY PARTICULAR PURPOSE or INTELLECTUAL PROPERTY
- * INFRINGEMENT. By making any use of this code, Recipient agrees that
- * no other licenses to any Intel patents, trade secrets, copyrights
- * or other intellectual property rights are granted herein, and no
- * other licenses shall arise by estoppel, implication or by operation
- * of law. Recipient accepts all risks of use.
- **************************************************************************/
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 /**
  * @file
---
+++ lib/libasim/include/asim/mod_numbers_dyn.h
@@ -0,0 +1,192 @@
+/*
+ * Copyright (C) 2003-2006 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef MOD_NUMBERS_H
+#define MOD_NUMBERS_H
+
+#define MAP_TO_INTERNAL(x) (int)((x) - min_value)
+#define MAP_TO_EXTERNAL(x) (int)((x) + min_value)
+class ModNumberDyn
+{
+  private:
+    int offset;
+    
+    int min_value;
+    int max_value;
+    unsigned int max_offset;
+
+    //  Check and correct wrapping
+    void correctWrap()
+    {
+        //  Since the correct 'offset' value is always in the
+        //  range [0..(max_value - min_value)], we can simply
+        //  use the modulo operator to correct wrapping problems
+
+        //  correct the 'past the high end' wrap
+        offset = offset % (max_offset + 1);
+
+        //  correct for the 'lower than the low end' wrap
+        if (offset < 0) {
+            offset = offset + (max_offset + 1);
+        }
+    }
+
+  public:
+    // default sets up a toggler
+    ModNumberDyn(int max=1, int min=0, int value=0)
+    {
+        SetMaxMin(max, min, value);
+    }
+
+    ModNumberDyn(const ModNumberDyn & x, int value)
+    {
+        *this = x;
+        Set(value);
+    }
+
+    ModNumberDyn Set(int value)
+    {
+        offset = MAP_TO_INTERNAL(value);
+        correctWrap();
+        
+        return *this;
+    }
+
+    ModNumberDyn Set(ModNumberDyn num)
+    {
+        offset = num.offset;
+        min_value = num.min_value;
+        max_value = num.max_value;
+        
+        return *this;
+    }
+    
+    int Get()
+    {
+        return MAP_TO_EXTERNAL(offset);
+    }
+
+    int GetMax() 
+    { 
+        return max_value; 
+    }
+    
+    int GetMin() 
+    { 
+        return min_value; 
+    }
+
+    void SetMaxMin(int max=1, int min=0, int initial_value=0)
+    {
+        min_value = min;
+        max_value = max;
+        
+        max_offset = max_value - min_value;
+        
+        offset = MAP_TO_INTERNAL(initial_value);
+        correctWrap();
+    }
+
+    ModNumberDyn Next()
+    {
+        ++offset;
+        correctWrap();
+        
+        return *this;
+    }
+
+    ModNumberDyn Prev()
+    {
+        --offset;
+        correctWrap();
+        
+        return *this;
+    }
+
+#if OPERATORS
+    // a + b == a.operator+(b)
+    int operator+(int delta) {}
+    int operator-(int delta) {}
+#endif
+
+    //  ++a  == a.operator++()     [prefix]
+    ModNumberDyn operator++()
+    {
+        ++offset;
+        correctWrap();
+
+        return *this;
+    }
+
+    ModNumberDyn operator--()
+    {
+        --offset;
+        correctWrap();
+
+        return *this;
+    }
+
+    //  a++  == a.operator++(int)  [postfix]
+    ModNumberDyn operator++(int dummy)
+    {
+        ModNumberDyn rv(*this);
+
+        ++offset;
+        correctWrap();
+
+        return rv;
+    }
+
+    ModNumberDyn operator--(int dummy)
+    {
+        ModNumberDyn rv(*this);
+
+        --offset;
+        correctWrap();
+
+        return rv;
+    }
+
+    //  Booleans
+    bool operator==(int value)
+    {
+        return (offset == (int)(MAP_TO_INTERNAL(value) % (max_offset + 1)));
+    }
+
+    bool operator==(ModNumberDyn num)
+    {
+        return (offset == num.offset);
+    }
+
+    bool operator!=(int value)
+    {
+        return (offset != (int)(MAP_TO_INTERNAL(value) % (max_offset + 1)));
+    }
+
+    bool operator!=(ModNumberDyn num)
+    {
+        return (offset != num.offset);
+    }
+};
+
+
+
+#undef MAP_TO_INTERNAL
+#undef MAP_TO_EXTERNAL
+
+#endif
